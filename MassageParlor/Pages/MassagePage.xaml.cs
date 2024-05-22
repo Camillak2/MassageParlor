276e5be13b1f5ed0de58de8cbdc586cb322004c5
Camillak2 <ksagdieva2@gmail.com>
2024-05-22 11:07:16 +0300

    .

--- MassageParlor/Pages/MassagePage.xaml.cs
+++ MassageParlor/Pages/MassagePage.xaml.cs
@@ -21,9 +21,37 @@ namespace MassageParlor.Pages
     /// </summary>
     public partial class MassagePage : Page
     {
+        private ToolTip tooltip = new ToolTip();
+
         public MassagePage()
         {
             InitializeComponent();
+            Image image1 = new Image();
+            image1.Source = new BitmapImage(new Uri("path/to/Images/body.png", UriKind.Relative)); // Замените путь на ваш файл изображения
+            image1.MouseEnter += Image_MouseEnter;
+            image1.MouseLeave += Image_MouseLeave;
+            image1.ToolTip = tooltip;
+
+            tooltip.Content = "Это описание для изображения 1.";
+        }
+
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Показать tooltip
+            Image image = (Image)sender;
+            Point mousePosition = e.GetPosition(image); // Получите позицию курсора над картинкой
+
+            // Настройка положения tooltip 
+            // (Например, разместите его справа от курсора)
+            tooltip.PlacementTarget = image;
+            tooltip.Placement = System.Windows.Controls.Primitives.PlacementMode.Right;
+            tooltip.IsOpen = true;
+        }
+
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            // Скрыть tooltip
+            tooltip.IsOpen = false;
         }
 
         private void ProfileBTN_Click(object sender, RoutedEventArgs e)
@@ -31,14 +59,9 @@ namespace MassageParlor.Pages
             NavigationService.Navigate(new MyPersonalAccountPage());
         }
 
-        private void WorkersBTN_Click(object sender, RoutedEventArgs e)
+        private void ServicesBTN_Click(object sender, RoutedEventArgs e)
         {
-            NavigationService.Navigate(new WorkersPage());
-        }
-
-        private void ClientsBTN_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService.Navigate(new ClientsPage());
+            NavigationService.Navigate(new ServicesPage());
         }
 
         private void RecordsBTN_Click(object sender, RoutedEventArgs e)
@@ -49,11 +72,6 @@ namespace MassageParlor.Pages
         private void MassageBTN_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new MassagePage());
-        }
-
-        private void ServicesBTN_Click(object sender, RoutedEventArgs e)
-        {
-            NavigationService.Navigate(new ServicesPage());
         }
 
         private void LogOutBTN_Click(object sender, RoutedEventArgs e)
@@ -70,5 +88,41 @@ namespace MassageParlor.Pages
             }
             //printDialog.PrintQueue.Dispose();
         }
+
+        //private void One_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Button button = (Button)sender;
+        //    One.Background = Brushes.Red;
+
+        //    Two.Background = Brushes.Gray;
+
+        //    Three.Background = Brushes.Gray;
+
+        //    hdh.Text = 1.ToString();
+        //}
+
+        //private void Two_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Button button = (Button)sender;
+        //    One.Background = Brushes.Red;
+
+        //    Two.Background = Brushes.Red;
+
+        //    Three.Background = Brushes.Gray;
+
+        //    hdh.Text = 2.ToString();
+        //}
+
+        //private void Three_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Button button = (Button)sender;
+        //    One.Background = Brushes.Red;
+
+        //    Two.Background = Brushes.Red;
+
+        //    Three.Background = Brushes.Red;
+
+        //    hdh.Text = 3.ToString();
+        //}
     }
 }
